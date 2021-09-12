99fe823dfcba4a0c0c6b65706abb6928b7640d02
benjamin <inufuni@naver.com>
2021-09-13 02:49:36 +0900

    sunday

---
+++ OneDrive/바탕 화면/JAVA8/sunday/sunday/src/main/java/com/example/sunday/SundayApplication.java
@@ -0,0 +1,13 @@
+package com.example.sunday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SundayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SundayApplication.class, args);
+	}
+
+}
---
+++ OneDrive/바탕 화면/JAVA8/sunday/sunday/src/test/java/com/example/sunday/SundayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sunday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SundayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
